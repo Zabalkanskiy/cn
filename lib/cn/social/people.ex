8407bb2d8afd5618b0ef26e15fedf7e5b48bc57f
Alexander <azabalkanskij@hotmail.com>
2019-08-03 18:16:42 +0300

    add people schema

---
+++ lib/cn/social/people.ex
@@ -0,0 +1,24 @@
+defmodule Cn.Social.People do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  alias Cn.Social.People
+
+  schema("people")do
+    field :name, :string
+    field :surname, :string
+    field :sex, :boolean
+    field :country, :string
+    field :about, :string
+    field :city, :string
+
+    timestamps()
+  end
+
+  def changeset(%People{}=people, attrs)do
+    people
+    |>cast(attrs, [:name,:surname,:sex,:country, :about, :city])
+    |>validate_required([:name,:surname,:sex,:country, :about, :city])
+  end
+
+end
